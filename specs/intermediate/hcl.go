9431d5ee5e0336693577f94b0d2a8b86f3699c35
Jeroen Rinzema <post@jeroen.no>
2020-02-06 14:16:20 +0100

    initial spec release

---
+++ specs/intermediate/hcl.go
@@ -0,0 +1,31 @@
+package intermediate
+
+import (
+	"errors"
+	"io"
+	"io/ioutil"
+
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/gohcl"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
+)
+
+// UnmarshalHCL unmarshals the given HCL stream into a intermediate resource.
+func UnmarshalHCL(filename string, reader io.Reader) (manifest *Manifest, _ error) {
+	bb, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	file, diags := hclsyntax.ParseConfig(bb, filename, hcl.InitialPos)
+	if diags.HasErrors() {
+		return nil, errors.New(diags.Error())
+	}
+
+	diags = gohcl.DecodeBody(file.Body, nil, &manifest)
+	if diags.HasErrors() {
+		return nil, errors.New(diags.Error())
+	}
+
+	return manifest, nil
+}
---
+++ specs/intermediate/intermediate.go
@@ -0,0 +1,99 @@
+package intermediate
+
+import (
+	"github.com/hashicorp/hcl/v2"
+)
+
+// Manifest intermediate specs
+type Manifest struct {
+	Flows      []Flow     `hcl:"flow,block"`
+	Middleware []Flow     `hcl:"middleware,block"`
+	Endpoints  []Endpoint `hcl:"endpoint,block"`
+	Services   []Service  `hcl:"service,block"`
+	Callers    []Caller   `hcl:"caller,block"`
+}
+
+// Flow intermediate specification
+type Flow struct {
+	Name   string        `hcl:"name,label"`
+	Input  *ParameterMap `hcl:"input,block"`
+	Calls  []Call        `hcl:"call,block"`
+	Output *ParameterMap `hcl:"output,block"`
+}
+
+// Endpoint intermediate specification
+type Endpoint struct {
+	Flow string   `hcl:"flow,label"`
+	Body hcl.Body `hcl:",remain"`
+}
+
+// Header represents a collection of key values
+type Header map[string]string
+
+// ParameterMap is the initial map of parameter names (keys) and their (templated) values (values)
+type ParameterMap struct {
+	Header     *Header                `hcl:"header,block"`
+	Message    []NestedParameterMap   `hcl:"message,block"`
+	Repeated   []RepeatedParameterMap `hcl:"repeated,block"`
+	Properties hcl.Body               `hcl:",remain"`
+}
+
+// NestedParameterMap is a map of parameter names (keys) and their (templated) values (values)
+type NestedParameterMap struct {
+	Name       string                 `hcl:"name,label"`
+	Message    []NestedParameterMap   `hcl:"message,block"`
+	Repeated   []RepeatedParameterMap `hcl:"repeated,block"`
+	Properties hcl.Body               `hcl:",remain"`
+}
+
+// RepeatedParameterMap is a map of repeated message blocks/values
+type RepeatedParameterMap struct {
+	Name       string                 `hcl:"name,label"`
+	Template   string                 `hcl:"template,label"`
+	Message    []NestedParameterMap   `hcl:"message,block"`
+	Repeated   []RepeatedParameterMap `hcl:"repeated,block"`
+	Properties hcl.Body               `hcl:",remain"`
+}
+
+// Call intermediate specification
+type Call struct {
+	Name     string        `hcl:"name,label"`
+	Endpoint string        `hcl:"endpoint,label"`
+	Type     string        `hcl:"type,optional"`
+	Request  *ParameterMap `hcl:"request,block"`
+	Rollback *RollbackCall `hcl:"rollback,block"`
+}
+
+// RollbackCall intermediate specification
+type RollbackCall struct {
+	Endpoint string       `hcl:"endpoint,label"`
+	Request  ParameterMap `hcl:"request,block"`
+}
+
+// Service specification
+type Service struct {
+	Alias  string `hcl:"alias,label"`
+	Caller string `hcl:"caller,label"`
+	Proto  string `hcl:"proto,optional"`
+}
+
+// Caller specification
+type Caller struct {
+	Name       string   `hcl:"name,label"`
+	Properties hcl.Body `hcl:",remain"`
+}
+
+// Proxy specification
+type Proxy struct {
+	Name    string        `hcl:"name,label"`
+	Calls   []Call        `hcl:"call,block"`
+	Forward ProxyForward  `hcl:"forward,block"`
+	Output  *ParameterMap `hcl:"output,block"`
+}
+
+// ProxyForward specification
+type ProxyForward struct {
+	Name     string        `hcl:"name,label"`
+	Endpoint string        `hcl:"endpoint,label"`
+	Rollback *RollbackCall `hcl:"rollback,block"`
+}
