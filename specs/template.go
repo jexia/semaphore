d7af2ff73c0e155ca34cd7a3d47c7cd95d97941d
Jeroen Rinzema <post@jeroen.no>
2020-02-09 00:29:31 +0100

    template refs

--- specs/template.go
+++ specs/template.go
@@ -1,5 +1,11 @@
 package specs
 
+import (
+	"strings"
+
+	"github.com/zclconf/go-cty/cty"
+)
+
 const (
 	// TemplateOpen tag
 	TemplateOpen = "{{"
@@ -27,6 +33,48 @@ const (
 	DefaultCallProperty = ResourceResponse
 )
 
+// IsTemplate checks whether the given value is a template
+func IsTemplate(value string) bool {
+	return strings.HasPrefix(value, TemplateOpen) && strings.HasSuffix(value, TemplateClose)
+}
+
+// GetTemplateContent trims the opening and closing tags from the given template value
+func GetTemplateContent(value string) string {
+	value = strings.Replace(value, TemplateOpen, "", 1)
+	value = strings.Replace(value, TemplateClose, "", 1)
+	value = strings.TrimSpace(value)
+	return value
+}
+
+// ParseReference parses the given value as a template reference
+func ParseReference(value string) *PropertyReference {
+	rv := strings.Split(value, ReferenceDelimiter)
+
+	reference := PropertyReference{
+		Resource: rv[0],
+		Label:    LabelOptional,
+	}
+
+	if len(rv) == 1 {
+		return &reference
+	}
+
+	reference.Path = rv[1]
+	return &reference
+}
+
+// SetTemplate parses the given value template and sets the resource and path
+func SetTemplate(property *Property, value cty.Value) {
+	if value.Type() != cty.String {
+		return
+	}
+
+	content := GetTemplateContent(value.AsString())
+
+	// Currently only references could be defined inside templates, possible features could be added in the future
+	property.Reference = ParseReference(content)
+}
+
 // JoinPath joins the given flow paths
 func JoinPath(values ...string) (result string) {
 	for _, value := range values {
@@ -50,4 +98,9 @@ func JoinPath(values ...string) (result string) {
 	}
 
 	return result
+}
+
+// SplitPath splits the given path into parts
+func SplitPath(path string) []string {
+	return strings.Split(path, PathDelimiter)
 }
---
+++ specs/template_test.go
@@ -0,0 +1,19 @@
+package specs
+
+import "testing"
+
+func TestJoinPath(t *testing.T) {
+	tests := map[string][]string{
+		"service.echo": {"service", "echo"},
+		"ping.pong":    {"ping.", "pong"},
+		"call.me":      {"call.", "me."},
+		"":             {"", ""},
+	}
+
+	for expected, input := range tests {
+		result := JoinPath(input...)
+		if result != expected {
+			t.Errorf("unexpected result: %s expected %s", result, expected)
+		}
+	}
+}
