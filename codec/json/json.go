f53d729130caa12a61bd0ecbbfb66508ec70ff44
Jeroen Rinzema <post@jeroen.no>
2020-02-24 15:37:54 +0100

    additional schema checks

--- codec/json/json.go
+++
@@ -1,9 +0,0 @@
-package json
-
-// func (message *Message) Marshal(*refs.Store) (io.Reader, error) {
-// 	return nil, nil
-// }
-
-// func (message *Message) Unmarshal(io.Reader, *refs.Store) error {
-// 	return nil, nil
-// }
