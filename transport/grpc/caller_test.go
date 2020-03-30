01b6dee1a701e2af2c320034367a2362a681c119
Jeroen Rinzema <post@jeroen.no>
2020-03-30 13:54:18 +0200

    gRPC test cases

---
+++ transport/grpc/caller_test.go
@@ -0,0 +1,90 @@
+package grpc
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/jexia/maestro/flow"
+	"github.com/jexia/maestro/instance"
+	"github.com/jexia/maestro/metadata"
+	"github.com/jexia/maestro/refs"
+	"github.com/jexia/maestro/schema"
+	"github.com/jexia/maestro/schema/mock"
+	"github.com/jexia/maestro/specs"
+	"github.com/jexia/maestro/transport"
+)
+
+func TestCaller(t *testing.T) {
+	ctx := instance.NewContext()
+	specs := &specs.Node{
+		Name: "first",
+	}
+
+	called := 0
+	call := NewCallerFunc(func(ctx context.Context, refs *refs.Store) error {
+		called++
+		return nil
+	})
+
+	nodes := flow.Nodes{
+		flow.NewNode(ctx, specs, call, nil),
+	}
+
+	listener, port := NewMockListener(t, nodes)
+
+	defer listener.Close()
+	go listener.Serve()
+
+	// Some CI pipelines take a little while before the listener is active
+	time.Sleep(100 * time.Millisecond)
+
+	constructor := NewCaller()
+	caller := constructor(ctx)
+
+	service := &mock.Service{
+		Name:      "mock",
+		Package:   "pkg",
+		Host:      fmt.Sprintf("127.0.0.1:%d", port),
+		Transport: "grpc",
+		Codec:     "proto",
+		Methods: map[string]*mock.Method{
+			"simple": {
+				Name:    "simple",
+				Input:   nil,
+				Output:  nil,
+				Options: schema.Options{},
+			},
+		},
+		Options: schema.Options{},
+	}
+
+	dial, err := caller.Dial(service, nil, schema.Options{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer dial.Close()
+
+	if len(dial.GetMethods()) != 1 {
+		t.Errorf("unexpected methods %+v", dial.GetMethods())
+	}
+
+	rw := transport.NewResponseWriter(bytes.NewBuffer([]byte{}))
+	rq := &transport.Request{
+		Header: metadata.MD{},
+		Method: dial.GetMethod("simple"),
+		Body:   bytes.NewBuffer([]byte{}),
+	}
+
+	err = dial.SendMsg(context.Background(), rw, rq, refs.NewStore(0))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if called != 1 {
+		t.Errorf("unexpected called %d, expected %d", called, len(nodes))
+	}
+}
---
+++ transport/grpc/grpc_test.go
@@ -0,0 +1,93 @@
+package grpc
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"testing"
+
+	"github.com/jexia/maestro/codec"
+	"github.com/jexia/maestro/flow"
+	"github.com/jexia/maestro/instance"
+	"github.com/jexia/maestro/refs"
+	"github.com/jexia/maestro/schema/mock"
+	"github.com/jexia/maestro/specs"
+	"github.com/jexia/maestro/specs/labels"
+	"github.com/jexia/maestro/specs/types"
+	"github.com/jexia/maestro/transport"
+)
+
+type caller struct {
+	fn func(context.Context, *refs.Store) error
+}
+
+func (caller *caller) Do(ctx context.Context, store *refs.Store) error {
+	return caller.fn(ctx, store)
+}
+
+func (caller *caller) References() []*specs.Property {
+	return nil
+}
+
+func NewCallerFunc(fn func(context.Context, *refs.Store) error) flow.Call {
+	return &caller{fn: fn}
+}
+
+func NewMockListener(t *testing.T, nodes flow.Nodes) (transport.Listener, int) {
+	port := AvailablePort(t)
+	addr := fmt.Sprintf(":%d", port)
+
+	ctx := instance.NewContext()
+	listener := NewListener(addr, nil)(ctx)
+
+	constructors := map[string]codec.Constructor{}
+	endpoints := []*transport.Endpoint{
+		{
+			Request: NewSimpleMockSpecs(),
+			Flow:    flow.NewManager(ctx, "test", nodes),
+			Options: specs.Options{
+				ServiceOption: "mock",
+				MethodOption:  "simple",
+				PackageOption: "pkg",
+			},
+			Response: NewSimpleMockSpecs(),
+		},
+	}
+
+	listener.Handle(endpoints, constructors)
+	return listener, port
+}
+
+func NewSimpleMockSpecs() *specs.ParameterMap {
+	return &specs.ParameterMap{
+		Property: &specs.Property{
+			Type:  types.Message,
+			Label: labels.Optional,
+			Nested: map[string]*specs.Property{
+				"message": {
+					Name:  "message",
+					Path:  "message",
+					Type:  types.String,
+					Label: labels.Optional,
+					Desciptor: &mock.Property{
+						Name:     "message",
+						Comment:  "mock",
+						Type:     types.String,
+						Label:    labels.Optional,
+						Position: 1,
+					},
+				},
+			},
+		},
+	}
+}
+
+func AvailablePort(t *testing.T) int {
+	listener, err := net.Listen("tcp", ":0")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer listener.Close()
+	return listener.Addr().(*net.TCPAddr).Port
+}
