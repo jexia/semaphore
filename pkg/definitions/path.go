2ffc4bec1eaeb5636f228a1f1f00a11c5df341ea
Jeroen Rinzema <post@jeroen.no>
2020-05-22 17:23:27 +0200

    improved symlink resolve and lookup

--- pkg/definitions/path.go
+++ pkg/definitions/path.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"strings"
 
 	"github.com/jexia/maestro/pkg/instance"
 	"github.com/jexia/maestro/pkg/logger"
@@ -29,18 +30,22 @@ func ResolvePath(ctx instance.Context, ignore []string, pattern string) (files [
 		resolved[path] = struct{}{}
 	}
 
-	return walk(ctx, resolved, pattern)
+	return walk(ctx, "", filepath.Dir(CleanPattern(pattern)), resolved, pattern)
 }
 
-func walk(ctx instance.Context, resolved map[string]struct{}, pattern string) (files []*FileInfo, _ error) {
-	dir := filepath.Dir(CleanPattern(pattern))
+func walk(ctx instance.Context, prefix string, root string, resolved map[string]struct{}, pattern string) (files []*FileInfo, _ error) {
 	pattern = filepath.Clean(pattern)
 
-	ctx.Logger(logger.Core).WithField("dir", dir).Debug("Resolve pattern")
+	ctx.Logger(logger.Core).WithField("dir", root).Debug("Resolve pattern")
 
-	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
+		}
+
+		if prefix != "" {
+			path = strings.TrimPrefix(path, root)
+			path = filepath.Join(prefix, path)
 		}
 
 		ctx.Logger(logger.Core).WithField("path", path).Debug("Matching path")
@@ -51,6 +56,33 @@ func walk(ctx instance.Context, resolved map[string]struct{}, pattern string) (f
 		}
 
 		resolved[path] = struct{}{}
+
+		if info.Mode()&os.ModeSymlink == os.ModeSymlink {
+			target, err := os.Readlink(path)
+			if err != nil {
+				return err
+			}
+
+			if !filepath.IsAbs(target) {
+				target = filepath.Join(filepath.Dir(path), target)
+			}
+
+			fi, err := os.Lstat(target)
+			if err != nil {
+				return err
+			}
+
+			if fi.IsDir() {
+				result, err := walk(ctx, path, target, resolved, pattern)
+				if err != nil {
+					return err
+				}
+
+				files = append(files, result...)
+
+				return nil
+			}
+		}
 
 		if info.IsDir() {
 			return nil
