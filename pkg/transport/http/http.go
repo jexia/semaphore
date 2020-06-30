3823143d239ac64f1b55ff83c4da00b2b6c4c811
Jeroen Rinzema <post@jeroen.no>
2020-06-30 14:09:55 +0200

    feat(reference): extended reference lookup regex to support @

--- pkg/transport/http/http.go
+++ pkg/transport/http/http.go
@@ -6,4 +6,4 @@ import (
 
 // ReferenceLookup is executed to lookup references within a given endpoint.
 // A reference starts with a colon and could contain characters, numbers, underscores and hyphens and dots to define nested properties
-var ReferenceLookup = regexp.MustCompile(`(?m):[a-zA-Z\d\_\-\.]+`)
+var ReferenceLookup = regexp.MustCompile(`(?m):[a-zA-Z\d@\_\-\.]+`)
