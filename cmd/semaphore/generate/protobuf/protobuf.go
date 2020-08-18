a959082cd9e8815c98ba579a8fcc13c4e921a80d
Alex Sch <alexander@jexia.com>
2020-08-18 14:24:03 +0300

    file output option

--- cmd/semaphore/generate/protobuf/protobuf.go
+++ cmd/semaphore/generate/protobuf/protobuf.go
@@ -2,7 +2,11 @@ package protobuf
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"path"
+	"path/filepath"
+	"strings"
 
 	"github.com/jexia/semaphore"
 	"github.com/jexia/semaphore/cmd/semaphore/config"
@@ -18,22 +22,27 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var params = config.New()
+var (
+	params = config.New()
 
-// Command represents the semaphore daemon command
-var Command = &cobra.Command{
-	Use:   "protobuf",
-	Short: "Generates a protobuf definitions",
-	Long: `Generates a protobuf definitions.
-*NOTE*: This is a experimental feature and not all features are supported yet.`,
-	RunE:         run,
-	SilenceUsage: true,
-}
+	// Command represents the semaphore daemon command
+	Command = &cobra.Command{
+		Use:   "protobuf",
+		Short: "Generates a protobuf definitions",
+		Long: `Generates a protobuf definitions.
+	*NOTE*: This is a experimental feature and not all features are supported yet.`,
+		RunE:         run,
+		SilenceUsage: true,
+	}
+
+	output string
+)
 
 func init() {
 	Command.PersistentFlags().StringSliceVar(&params.Protobuffers, "proto", []string{}, "If set are all proto definitions found inside the given path passed as schema definitions, all proto definitions are also passed as imports")
 	Command.PersistentFlags().StringSliceVarP(&params.Files, "file", "f", []string{"config.hcl"}, "Parses the given file as a definition file")
 	Command.PersistentFlags().StringVar(&params.LogLevel, "level", "warn", "Global logging level, this value will override the defined log level inside the file definitions")
+	Command.PersistentFlags().StringVarP(&output, "output", "o", "", "Output directory")
 }
 
 func run(cmd *cobra.Command, args []string) error {
@@ -71,17 +80,39 @@ func run(cmd *cobra.Command, args []string) error {
 	printer := &protoprint.Printer{}
 
 	for key, service := range services {
+
+		dst, err := getOutput(output, key)
+		if err != nil {
+			return fmt.Errorf("failed to set the output for generator: %w", err)
+		}
+
 		descriptor, err := service.FileDescriptor()
 		if err != nil {
 			return fmt.Errorf("cannot generate file descriptor for %q: %w", key, err)
 		}
 
-		if err := printer.PrintProtoFile(descriptor, os.Stdout); err != nil {
+		if err := printer.PrintProtoFile(descriptor, dst); err != nil {
 			return err
 		}
+
+		dst.Close()
 	}
 
 	return nil
+}
+
+func getOutput(output, pkg string) (io.WriteCloser, error) {
+	if output == "" {
+		return os.Stdout, nil
+	}
+
+	filePath := path.Join(append([]string{output}, strings.Split(pkg, ".")...)...) + ".proto"
+
+	if err := os.MkdirAll(filepath.Dir(filePath), os.ModePerm); err != nil {
+		return nil, err
+	}
+
+	return os.Create(filePath)
 }
 
 func generate(ctx *broker.Context, endpoints transport.EndpointList) (map[string]*proto.Service, error) {
