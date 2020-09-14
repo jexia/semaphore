3af4fe014217ee9466296ac3d5fb669ff83bc057
Alex Sch <alexander@jexia.com>
2020-09-14 14:38:26 +0300

    cleanup

--- cmd/semaphore/functions/strings/sprintf/token.go
+++ cmd/semaphore/functions/strings/sprintf/token.go
@@ -5,30 +5,13 @@ import (
 	"strconv"
 )
 
-const (
-	// TConstant is a constant token which should be printed as is.
-	TConstant Kind = iota
-	// TPrecision is a token that is not printed itself but is a part of verb.
-	TPrecision
-	// TVerb is a token which contains the format verb.
-	TVerb
-)
-
-// Kind represents token kind.
-type Kind int
-
 // Token represents one of the available tokens.
 type Token interface {
 	fmt.Stringer
-
-	Kind() Kind
 }
 
 // Constant is a token that does not need any formatting.
 type Constant string
-
-// Kind returns token kind.
-func (Constant) Kind() Kind { return TConstant }
 
 func (c Constant) String() string { return string(c) }
 
@@ -39,9 +22,6 @@ type Verb struct {
 	Formatter Formatter
 }
 
-// Kind returns token kind.
-func (Verb) Kind() Kind { return TVerb }
-
 func (v Verb) String() string { return v.Verb }
 
 // Precision is a token which describes the precision and used to create a verb.
@@ -49,9 +29,6 @@ type Precision struct {
 	Width int64
 	Scale int64
 }
-
-// Kind returns token kind.
-func (Precision) Kind() Kind { return TPrecision }
 
 func (p Precision) String() string {
 	return "%" + strconv.FormatInt(p.Width, 10) + "." + strconv.FormatInt(p.Scale, 10)
