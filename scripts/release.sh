474495d4a9942bc3bf069636cc61bf8ec0c73911
Jeroen Rinzema <post@jeroen.no>
2020-10-06 16:17:13 +0200

    feat: updated release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,6 +3,12 @@
 # abort on errors
 set -e
 
+remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
+git config --local user.email "action@github.com"
+git config --local user.name "GitHub Action"
+git remote add publisher "${remote_repo}"
+
 npm install
 
 # build
@@ -16,4 +22,4 @@ cd ./docs
 
 git add .
 git commit -m 'build: 🏗️ automatically generated documentation'
-git push
+git push publisher ${BRANCH}
