d9c4ea6a826a2e3ae80cbf25a43b99039dfcd5b6
Jeroen Rinzema <post@jeroen.no>
2020-10-09 17:53:59 +0200

    feat: deploy to gh-pages

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,23 +3,16 @@
 # abort on errors
 set -e
 
-remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-
-git config --local user.email "action@github.com"
-git config --local user.name "GitHub Action"
-git remote add publisher "${remote_repo}"
-
 npm install
 
 # build
 npm run build
 
-# navigate into the build output directory
-cd ./docs
-
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git add .
+# force add due to ignored in .gitignore
+git add -f ./dist
+
 git commit -m 'build: 🏗️ automatically generated documentation'
-git push publisher ${BRANCH}
+git subtree push --prefix dist origin gh-pages
