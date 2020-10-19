62ee3168dde0b0e4d991a07e9abaacf11793f8e5
Jeroen Rinzema <post@jeroen.no>
2020-10-19 17:09:15 +0200

    feat: git user, email

--- website/scripts/release.sh
+++ website/scripts/release.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 
-cd website
-
 # abort on errors
 set -e
+
+git config --global user.email "actions@github.com"
+git config --global user.name "Github Actions"
 
 # https://v2.docusaurus.io/docs/deployment#deploying-to-github-pages
 export GIT_USER="github-actions"
