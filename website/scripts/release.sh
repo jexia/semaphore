5bd7788808f7733569d3a0dd2ba34f79c10cc6ba
Jeroen Rinzema <post@jeroen.no>
2020-10-20 12:03:58 +0200

    fix: remove and build

--- website/scripts/release.sh
+++ website/scripts/release.sh
@@ -9,9 +9,7 @@ npm run build
 git config --global user.email "action@github.com"
 git config --global user.name "GitHub Action"
 
-git clone -b docs https://github.com/jexia/semaphore.git
+git clone -b docs https://github.com/jexia/semaphore.git semaphore
 
-rm -r ./semaphore/*
-mv ./build/* ./semaphore
-
+(cd semaphore; rm -r *; mv ../build/* ./)
 (cd semaphore; git add -A; git commit -m 'build: 🏗️ automatically generated documentation'; git push)
