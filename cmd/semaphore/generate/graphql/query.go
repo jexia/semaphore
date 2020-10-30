8c0f7a69dffddffa112eeb56222f2f6e53e4f2ab
Ashmeet Singh <hackercompany@gmail.com>
2020-10-30 03:36:30 +0100

    query file

---
+++ cmd/semaphore/generate/graphql/query.go
@@ -0,0 +1,92 @@
+package graphql
+
+const query = `
+fragment FullType on __Type {
+	kind
+	name
+	fields(includeDeprecated: true) {
+	  name
+	  args {
+		...InputValue
+	  }
+	  type {
+		...TypeRef
+	  }
+	  isDeprecated
+	  deprecationReason
+	}
+	inputFields {
+	  ...InputValue
+	}
+	interfaces {
+	  ...TypeRef
+	}
+	enumValues(includeDeprecated: true) {
+	  name
+	  isDeprecated
+	  deprecationReason
+	}
+	possibleTypes {
+	  ...TypeRef
+	}
+  }
+  fragment InputValue on __InputValue {
+	name
+	type {
+	  ...TypeRef
+	}
+	defaultValue
+  }
+  fragment TypeRef on __Type {
+	kind
+	name
+	ofType {
+	  kind
+	  name
+	  ofType {
+		kind
+		name
+		ofType {
+		  kind
+		  name
+		  ofType {
+			kind
+			name
+			ofType {
+			  kind
+			  name
+			  ofType {
+				kind
+				name
+				ofType {
+				  kind
+				  name
+				}
+			  }
+			}
+		  }
+		}
+	  }
+	}
+  }
+  query IntrospectionQuery {
+	__schema {
+	  queryType {
+		name
+	  }
+	  mutationType {
+		name
+	  }
+	  types {
+		...FullType
+	  }
+	  directives {
+		name
+		locations
+		args {
+		  ...InputValue
+		}
+	  }
+	}
+  }
+`
