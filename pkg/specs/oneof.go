2f45961bd9d63f4cf575b11591dea1fb8605e5a2
Alex Sch <alexander@jexia.com>
2020-11-12 10:04:34 +0200

    reuse Message.Compare for OneOf

--- pkg/specs/oneof.go
+++ pkg/specs/oneof.go
@@ -1,18 +1,13 @@
 package specs
 
-import (
-	"errors"
-	"fmt"
-)
-
-// OneOf is a mixed type to let the schema validate values against exactly one of the templates.
+// OneOf is a mixed type to let the schema validate values against exactly one of the properties.
 // Example:
 // OneOf{
-//   {Scalar: &Scalar{Type: types.String}},
-//   {Scalar: &Scalar{Type: types.Int32}},
-//   {Message: &Message{...}},
+//   "string":{Scalar: &Scalar{Type: types.String}},
+//   "number":{Scalar: &Scalar{Type: types.Int32}},
+//   "object":{Message: &Message{...}},
 // }
-// A given value must be one of these types: string, int32 or the message.
+// A given value must be one of these types: string, number or an object.
 type OneOf map[string]*Property
 
 func (oneOf OneOf) String() string { return dump(oneOf) }
@@ -24,28 +19,5 @@ func (oneOf OneOf) Clone() OneOf {
 
 // Compare checks whether given OneOf mathches the provided one.
 func (oneOf OneOf) Compare(expected OneOf) error {
-	if expected == nil && oneOf == nil {
-		return nil
-	}
-
-	if expected == nil && oneOf != nil {
-		return errors.New("expected to be nil")
-	}
-
-	if expected != nil && oneOf == nil {
-		return fmt.Errorf("expected not to be nil")
-	}
-
-	for key, property := range oneOf {
-		nested, ok := expected[key]
-		if !ok {
-			return fmt.Errorf("unknown choice '%s'", key)
-		}
-
-		if err := property.Compare(nested); err != nil {
-			return fmt.Errorf("'oneof' choice mismatch: %w", err)
-		}
-	}
-
-	return nil
+	return Message(oneOf).Compare(Message(expected))
 }
